78f6cbbd543e04358a60ee6e9a92e814535fd84c
Tienkaca <tienkaca@gmail.com>
2022-05-06 20:58:11 +0700

    delete build,devel

--- uart_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tien/DATN2022/uart_ws/src
-# Build directory: /home/tien/DATN2022/uart_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("uart")
