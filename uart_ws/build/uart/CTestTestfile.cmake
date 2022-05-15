6cba2ae15469414782c8a952e4d7a10b31f4a882
Tienkaca <tienkaca@gmail.com>
2022-05-15 12:12:48 +0000

    rm all

--- uart_ws/build/uart/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/DATN2022/uart_ws/src/uart
-# Build directory: /home/ubuntu/DATN2022/uart_ws/build/uart
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
