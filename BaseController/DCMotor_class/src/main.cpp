383c47cd342c8d5a024affd34929cb4495c461bd
Tienkaca <68862808+Tienkaca@users.noreply.github.com>
2022-05-27 22:52:43 -0700

    2 wheel ok

--- BaseController/DCMotor_class/src/main.cpp
+++ BaseController/DCMotor_class/src/main.cpp
@@ -9,21 +9,32 @@ const int in1[2] = {6,10};
 const int in2[2] = {7,11};
 const int enA[2] = {2,3};
 const int enB[2] = {8,4};
+// // const int pwm = 5;
+// // const int in1=6;
+// // const int in2 = 7;
+// // const int enA = 2;
+// // const int enB = 8;
+// const int pwm = 9;
+// const int in1  =10;
+// const int in2 = 11;
+// const int enA = 3;
+// const int enB = 4;
 // used in an interrupt
 volatile int pos_i[2] = {0};
 volatile float velocity_i[2]= {0};
 volatile long prevT_i[2]= {0};
 
 DCMotor m0(pwm[0],in1[0],in2[0],enA[0],enB[0],85);
-DCMotor m1(pwm[0],in1[0],in2[0],enA[0],enB[0],78);
+DCMotor m1(pwm[1],in1[1],in2[1],enA[1],enB[1],85);
+
 void ISR0();
 void ISR1();
 void setup() {
   Serial.begin(76800);
   m0.SetUp(ISR0);
-  m0.SetVel(-20); 
+  m0.SetVel(-30); 
   m1.SetUp(ISR1);
-  m1.SetVel(-20);
+  m1.SetVel(-30);
 }
 
 void loop() {
@@ -64,7 +75,7 @@ void ISR0(){
 }
 void ISR1(){
   // Read encoder B when ENCA rises
-  int b = digitalRead(enB[0]);
+  int b = digitalRead(enB[1]);
   int increment = 0;
   if(b>0){
     // If B is high, increment forward
